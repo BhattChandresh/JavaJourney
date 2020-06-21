a7232710726e1904eae73feb96c263080f99409a
Chandresh Bhatt <chandreshvbhatt@gmail.com>
2020-06-21 18:22:05 +0530

    Design Improvement - Chess Program

--- JavaPrograms/src/main/java/com/practice/java/CodingInterview/Technogise/Bishop.java
+++ JavaPrograms/src/main/java/com/practice/java/CodingInterview/Technogise/Bishop.java
@@ -7,8 +7,9 @@ public class Bishop extends Piece {
     int x;
     int y;
     List<String> list = new ArrayList<String>();
+
     Bishop(String piece, String position) {
-        super(piece,position);
+        super(piece, position);
         x = getX(position);
         y = getY(position);
     }
@@ -18,51 +19,70 @@ public class Bishop extends Piece {
         int newX = 0;
         int newY = 0;
         int index = 0;
+        getDiagonalTopRightPositions(x,y);
+        getDiagonalBottomLeftPositions(x,y);
+        getDiagonalBottomRightPositions(x,y);
+        getDiagonalTopLeftPositions(x,y);
+        displayPossiblePositions(list);
+    }
 
-        while(index < 4) {
-            if(!(newX < ChessBoardSimulation.MIN_BOUND || newX > ChessBoardSimulation.MAX_BOUND || newY < ChessBoardSimulation.MIN_BOUND || newY > ChessBoardSimulation.MAX_BOUND)) {
-                if(index == 0) {
-                    newX -= 1;
-                    newY += 1;
-                    if((newX < ChessBoardSimulation.MIN_BOUND) || (newY > ChessBoardSimulation.MAX_BOUND))
-                        index = index + 1;
-                    list.add(Integer.toString(newX) + Integer.toString(newY));
-                }
-                if(index == 1) {
-                    newX += 1;
-                    newY -=1;
-                    if((newX > ChessBoardSimulation.MAX_BOUND) || (newY < ChessBoardSimulation.MIN_BOUND))
-                        index = index + 1;
-                    list.add(Integer.toString(newX) + Integer.toString(newY));
-                }
-                if(index == 2) {
-                    newX += 1;
-                    newY +=1;
-                    if((newX > ChessBoardSimulation.MAX_BOUND) || (newY > ChessBoardSimulation.MAX_BOUND))
-                        index = index + 1;
-                    list.add(Integer.toString(newX) + Integer.toString(newY));
-                }
-                if(index == 3){
-                    newX -= 1;
-                    newY -=1;
-                    if((newX < ChessBoardSimulation.MIN_BOUND) || (newY < ChessBoardSimulation.MIN_BOUND))
-                        break;
-                    list.add(Integer.toString(newX) + Integer.toString(newY));
-                }
+    public void getDiagonalTopRightPositions(int x, int y ) {
+        int newX, newY = 0;
+        newX = x;
+        newY = y;
+        while (true) {
+            if (!((newX < ChessBoardSimulation.MIN_BOUND || newX > ChessBoardSimulation.MAX_BOUND) || (newY < ChessBoardSimulation.MIN_BOUND || newY > ChessBoardSimulation.MAX_BOUND))) {
+                newX -= 1;
+                newY += 1;
+                if ((newX < ChessBoardSimulation.MIN_BOUND) || (newY > ChessBoardSimulation.MAX_BOUND))
+                    break;
+                list.add(Integer.toString(newX) + Integer.toString(newY));
+            }
+        }
+    }
+
+    public void getDiagonalBottomLeftPositions(int x, int y) {
+        int newX, newY = 0;
+        newX = x;
+        newY = y;
+        while (true) {
+            if (!((newX < ChessBoardSimulation.MIN_BOUND || newX > ChessBoardSimulation.MAX_BOUND) || (newY < ChessBoardSimulation.MIN_BOUND || newY > ChessBoardSimulation.MAX_BOUND))) {
+                newX += 1;
+                newY -= 1;
+                if ((newX > ChessBoardSimulation.MAX_BOUND) || (newY < ChessBoardSimulation.MIN_BOUND))
+                    break;
+                list.add(Integer.toString(newX) + Integer.toString(newY));
+            }
+        }
+    }
+
+    public void getDiagonalBottomRightPositions(int x, int y) {
+        int newX, newY = 0;
+        newX = x;
+        newY = y;
+        while (true) {
+            if (!((newX < ChessBoardSimulation.MIN_BOUND || newX > ChessBoardSimulation.MAX_BOUND) || (newY < ChessBoardSimulation.MIN_BOUND || newY > ChessBoardSimulation.MAX_BOUND))) {
+                newX += 1;
+                newY += 1;
+                if ((newX > ChessBoardSimulation.MAX_BOUND) || (newY > ChessBoardSimulation.MAX_BOUND))
+                    break;
+                list.add(Integer.toString(newX) + Integer.toString(newY));
+            }
+        }
+    }
+
+    public void getDiagonalTopLeftPositions(int x, int y) {
+        int newX, newY = 0;
+        newX = x;
+        newY = y;
+        while (true) {
+            if (!((newX < ChessBoardSimulation.MIN_BOUND || newX > ChessBoardSimulation.MAX_BOUND) || (newY < ChessBoardSimulation.MIN_BOUND || newY > ChessBoardSimulation.MAX_BOUND))) {
+                newX -= 1;
+                newY -= 1;
+                if ((newX < ChessBoardSimulation.MIN_BOUND) || (newY < ChessBoardSimulation.MIN_BOUND))
+                    break;
+                list.add(Integer.toString(newX) + Integer.toString(newY));
             }
         }
     }
 }
-
-//public class Bishop extends PiecePositions {
-//    public void getAllPossibleCellNumbers(int row, int column) {
-//        List<String> newPositions = new ArrayList<String>();
-//        int x = row;
-//        int y = column;
-//        getDiagonalTopRightPositions(x,y,newPositions);
-//        getDiagonalBottomLeftPositions(x,y,newPositions);
-//        getDiagonalBottomRightPositions(x,y,newPositions);
-//        getDiagonalTopLeftPositions(x,y,newPositions);
-//        displayAllPositions(newPositions);
-//    }
-//}
--- JavaPrograms/src/main/java/com/practice/java/CodingInterview/Technogise/Horse.java
+++ JavaPrograms/src/main/java/com/practice/java/CodingInterview/Technogise/Horse.java
@@ -32,12 +32,4 @@ public class Horse extends Piece {
 }
 
 
-//public class Horse extends PiecePositions {
-//    public void getAllPossibleCellNumbers(int row, int column) {
-//        List<String> newPositions = new ArrayList<String>();
-//        int x = row;
-//        int y = column;
-//        getHorsePositions(x,y, newPositions);
-//        displayAllPositions(newPositions);
-//    }
-//}
+
