86681f650f85c051031e5b2b81677a1a5da50118
Chandresh Bhatt <chandreshvbhatt@gmail.com>
2020-06-26 18:48:21 +0530

    Count No of 1s in given Number

---
+++ JavaPrograms/src/main/java/com/practice/java/mypractice/CountOneInNumber.java
@@ -0,0 +1,30 @@
+/**
+ * Count number of 1’s in binary representation of a number.
+ */
+
+package com.practice.java.mypractice;
+
+import com.practice.java.Author;
+
+import java.util.Scanner;
+
+@Author("Chandresh Bhatt")
+public class CountOneInNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        countOnes(number);
+    }
+
+    static void countOnes(int number) {
+        int count = 0;
+        int iteration = 0;
+        while(number != 0) {
+            number =  number & (number -1);
+            iteration++;
+             count++;
+        }
+        System.out.println("No of 1's in Number = " + count);
+        System.out.println("No. of Iteration = " + iteration);
+    }
+}
