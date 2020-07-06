11c04435e29c61f5cae2430ac33ca9a9b24164dd
Chandresh Bhatt <chandreshvbhatt@gmail.com>
2020-07-06 11:03:00 +0530

    Improvement as per requirement

--- JavaPrograms/src/main/java/com/practice/java/InterviewCoding/MatricsRowColToZero.java
+++ JavaPrograms/src/main/java/com/practice/java/InterviewCoding/MatricsRowColToZero.java
@@ -2,9 +2,12 @@ package com.practice.java.InterviewCoding;
 
 import com.practice.java.Author;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Author("Chandresh Bhatt")
-public class MatricsAllRowToZero {
-  //    static int[][] matrics = {
+public class MatricsRowColToZero {
+//      static int[][] matrics = {
 //            {12, 0, 34, 45, 89, 8},
 //            {1, 23, 56, 78, 9, 1},
 //            {23, 1, 8, 0, 76, 3},
@@ -19,6 +22,8 @@ public class MatricsAllRowToZero {
             {1,1,1,1,1},
             {0,1,1,1,1}
     };
+
+    static List<Integer> list = new ArrayList<Integer>();
 
     public static void main(String[] args) {
         printTheMatrics(matrics);
@@ -38,26 +43,44 @@ public class MatricsAllRowToZero {
         for (int row = 0; row < matrics.length; row++) {
            for (int col = 0; col < matrics.length; col++) {
                 if (matrics[row][col] == 0) {
-                    convertToZero(row, matrics.length);
+                    if(!(list.contains(col)))
+                        list.add(col);
+                    convertRowValueToZero(row, matrics.length);
+                 break;
                 }
             }
         }
         System.out.println();
+        System.out.println("Columns which has 0 element = " + list);
+        convertColValueToZero(list);
+        System.out.println();
+        publishFinalResult();
+    }
+
+    static void convertRowValueToZero(int row, int len) {
+        int col = 0;
+        while (len > 0) {
+            matrics[row][col] = 0;
+            col = col + 1;
+            len = len - 1;
+        }
+    }
+
+    static void convertColValueToZero(List list) {
+        for(int row=0;row < matrics.length;row++) {
+            for(int col=0; col < list.size();col++) {
+                matrics[row][(int) list.get(col)] = 0;
+            }
+        }
+    }
+
+    static void publishFinalResult() {
         System.out.println("Result is : ");
         for(int row=0;row < matrics.length;row++) {
             System.out.println();
             for(int col=0;col < matrics.length;col++) {
                 System.out.print(matrics[row][col] + " ");
             }
-        }
-    }
-
-    static void convertToZero(int row, int len) {
-        int col = 0;
-        while (len > 0) {
-            matrics[row][col] = 0;
-            col = col + 1;
-            len = len - 1;
         }
     }
 }
