0b9faa9fabbc4072b1653d50ec1d471bf5f721fc
Chandresh Bhatt <chandreshvbhatt@gmail.com>
2021-06-17 10:30:45 +0530

    Remove additional variable

--- JavaPrograms/src/main/java/com/practice/java/mypractice/RecursionRevStr.java
+++ JavaPrograms/src/main/java/com/practice/java/mypractice/RecursionRevStr.java
@@ -8,8 +8,8 @@ public class RecursionRevStr {
     public static String result = "";
 
     public static void main(String[] args) {
-        String str = "ABCD";
-        //String str = "String Reverse With Recursion";
+        //String str = "ABCD";
+        String str = "String Reverse With Recursion";
         int len = str.length() - 1;
         String rev = reverse(str, len);
         System.out.println("Reverse String is : " + rev);
@@ -17,9 +17,7 @@ public class RecursionRevStr {
 
     public static String reverse(String str, int len) {
         if (len > -1) {
-            int index = len;
-            result = result + str.charAt(index);
-//            System.out.println(len + "-> " + result);
+            result = result + str.charAt(len);
             reverse(str, len - 1);
         }
         return result;
