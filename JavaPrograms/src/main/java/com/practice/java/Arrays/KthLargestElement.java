a01ca2db766a8007518e88067a92569612aad48b
Chandresh Bhatt <chandreshvbhatt@gmail.com>
2021-06-20 15:50:39 +0530

    Find the Kth Largest element and it's JUnit test cases

---
+++ JavaPrograms/src/main/java/com/practice/java/Arrays/KthLargestElement.java
@@ -0,0 +1,30 @@
+/*
+Given an array and a number k where k is smaller than the size of the array,
+ we need to find the k’th smallest element in the given array. It is given that all array elements are distinct.
+ Examples:
+
+Input: arr[] = {7, 10, 4, 3, 20, 15}
+       k = 3
+Output: 7
+
+Input: arr[] = {7, 10, 4, 3, 20, 15}
+       k = 4
+Output: 10
+ */
+package com.practice.java.Arrays;
+
+import java.util.Arrays;
+
+public class KthLargestElement {
+    public static void main(String[] args) {
+        KthLargestElement kthLargestElement = new KthLargestElement();
+        int[] arr = {7, 10, 4, 3, 20, 15};
+        int desiredElement = kthLargestElement.getKthLargestElementBySoring(arr, 6);
+        System.out.println("Kth Largest element is = " + desiredElement);
+    }
+
+    int getKthLargestElementBySoring(int[] arr, int k) {
+        Arrays.sort(arr);
+        return arr[k - 1];
+    }
+}
---
+++ JavaPrograms/src/test/java/com/practice/java/Arrays/KthLargestElementTest.java
@@ -0,0 +1,25 @@
+package com.practice.java.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KthLargestElementTest {
+    private KthLargestElement kthLargestElement;
+
+    @BeforeEach
+    public void init() {
+        kthLargestElement = new KthLargestElement();
+    }
+
+    @Test
+    void testGetKthLargestElementBySoring() {
+        assertEquals(7, kthLargestElement.getKthLargestElementBySoring(new int[]{7, 10, 4, 3, 20, 15}, 3));
+        assertEquals(10, kthLargestElement.getKthLargestElementBySoring(new int[]{7, 10, 4, 3, 20, 15}, 4));
+        assertEquals(3, kthLargestElement.getKthLargestElementBySoring(new int[]{7, 10, 4, 3, 20, 15}, 1));
+        assertEquals(15, kthLargestElement.getKthLargestElementBySoring(new int[]{7, 10, 4, 3, 20, 15}, 5));
+        assertEquals(4, kthLargestElement.getKthLargestElementBySoring(new int[]{7, 10, 4, 3, 20, 15}, 2));
+        assertEquals(20, kthLargestElement.getKthLargestElementBySoring(new int[]{7, 10, 4, 3, 20, 15}, 6));
+    }
+}
