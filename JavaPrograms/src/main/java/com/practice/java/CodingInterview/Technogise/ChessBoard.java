6796c0a9fa6a15b0ee8d75c9cbc6b83d35e6de6e
Chandresh Bhatt <chandreshvbhatt@gmail.com>
2021-06-22 13:50:49 +0530

    Rectify the spelling mistake 'Because' and close the Scanner class object

--- JavaPrograms/src/main/java/com/practice/java/CodingInterview/Technogise/ChessBoard.java
+++ JavaPrograms/src/main/java/com/practice/java/CodingInterview/Technogise/ChessBoard.java
@@ -1,9 +1,9 @@
-/**
- * Version : 1.0
- * Author : Chandresh Bhatt
- * Objective :
- * Convert the Cell Number (H1) received from user to Co-ordinates (0,0) for  internal processing.
- * After processing, convert the Co-ordinates(7,7) to Cell Number(A8) in order to display the possible cell numbers to user.
+/*
+  Version : 1.0
+  Author : Chandresh Bhatt
+  Objective :
+  Convert the Cell Number (H1) received from user to Co-ordinates (0,0) for  internal processing.
+  After processing, convert the Co-ordinates(7,7) to Cell Number(A8) in order to display the possible cell numbers to user.
  */
 package com.practice.java.CodingInterview.Technogise;
 
@@ -12,36 +12,44 @@ import java.util.Map;
 
 public class ChessBoard {
     // Declaration for 8X8 Grid
-    private static int[][] grid = new int[8][8];
+    private static final int[][] grid = new int[8][8];
     //Identify the cell number a name
-    static Map<String, String> cellPositionToCoordinate = new HashMap<String, String>();
-    static Map<String, String> CoordinateToCellPosition = new HashMap<String, String>();
+    static Map<String, String> cellPositionToCoordinate = new HashMap<>();
+    static Map<String, String> CoordinateToCellPosition = new HashMap<>();
 
     // Create and Initialize the 8x8 Grid
     static {
         System.out.println("Initializing the Chess Board....");
-        String cellRow = "";
+        String cellRow;
         for (int row = 0; row < 8; row++) {
-            if (row == 0)
+            if (row == 0) {
                 cellRow = "H";
-            else if (row == 1)
+            }
+            else if (row == 1) {
                 cellRow = "G";
-            else if (row == 2)
+            }
+            else if (row == 2) {
                 cellRow = "F";
-            else if (row == 3)
+            }
+            else if (row == 3) {
                 cellRow = "E";
-            else if (row == 4)
+            }
+            else if (row == 4) {
                 cellRow = "D";
-            else if (row == 5)
+            }
+            else if (row == 5) {
                 cellRow = "C";
-            else if (row == 6)
+            }
+            else if (row == 6) {
                 cellRow = "B";
-            else if (row == 7)
+            }
+            else {
                 cellRow = "A";
+            }
             for (int column = 0; column < 8; column++) {
                 grid[row][column] = 0;
-                String input = Integer.toString(row) + Integer.toString(column);
-                cellPositionToCoordinate.put((cellRow + (column + 1)), (Integer.toString(row) + Integer.toString(column)));
+                String input = String.valueOf(row) + String.valueOf(column);
+                cellPositionToCoordinate.put((cellRow + (column + 1)), String.valueOf(row + column));
                 CoordinateToCellPosition.put(input, (cellRow + (column + 1)));
             }
         }
@@ -51,25 +59,33 @@ public class ChessBoard {
     // For production use this method is not required.
     // This is for testing purpose only.
     public void testInitializationOfChessBoard() {
-        String cellRow = "";
+        String cellRow;
         for (int row = 0; row < 8; row++) {
             System.out.println();
-            if (row == 0)
+            if (row == 0) {
                 cellRow = "H";
-            else if (row == 1)
+            }
+            else if (row == 1) {
                 cellRow = "G";
-            else if (row == 2)
+            }
+            else if (row == 2) {
                 cellRow = "F";
-            else if (row == 3)
+            }
+            else if (row == 3) {
                 cellRow = "E";
-            else if (row == 4)
+            }
+            else if (row == 4) {
                 cellRow = "D";
-            else if (row == 5)
+            }
+            else if (row == 5) {
                 cellRow = "C";
-            else if (row == 6)
+            }
+            else if (row == 6) {
                 cellRow = "B";
-            else if (row == 7)
+            }
+            else {
                 cellRow = "A";
+            }
             for (int column = 0; column < 8; column++) {
                 //System.out.print("grid["+ row + "][" + column + "] : " + cellPositionToCoordinate.get(cellRow + (column+1)) + " | "  + (cellRow + (column+1)));
                 //System.out.print("[" + row + "][" + column + "] : " + cellPositionToCoordinate.get(cellRow + (column+1)) + " | "  + (cellRow + (column+1)) + " || ");
--- JavaPrograms/src/main/java/com/practice/java/CodingInterview/Technogise/README.txt
+++ JavaPrograms/src/main/java/com/practice/java/CodingInterview/Technogise/README.txt
@@ -6,7 +6,7 @@
 - For King,Queen,Rook,Horse and Bishop chess board is always empty.
 - For Pawn, only forward direction is considered (A -> H) and it can not move from H -> A.
 - Hence we can't place pawn from A1 to A8.
-- Becasue the Chess Board is empty, no diagonal movement of Pawn to eliminate the enemy.
+- Because the Chess Board is empty, no diagonal movement of Pawn to eliminate the enemy.
 
 ## No external / third party libraries are used.
 
