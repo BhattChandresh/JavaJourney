d95e999f6439bef2be679114739660f7a48795b0
Chandresh Bhatt <chandreshvbhatt@gmail.com>
2021-07-07 15:10:44 +0530

    Program and it's JUnit test case for FizzBuzz

---
+++ JavaPrograms/src/main/java/com/practice/java/interviewcoding/FizzBuzz.java
@@ -0,0 +1,41 @@
+/*
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i if non of the above conditions are true.
+ */
+package com.practice.java.interviewcoding;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        sc.close();
+        String[] str = fizzBuzz.fizzOrBuzzOrFizzBuzz(number);
+        System.out.println(Arrays.toString(str));
+    }
+
+    protected String[] fizzOrBuzzOrFizzBuzz(int number) {
+        String[] result = new String[number];
+        String strResult;
+        for (int index = 1; index <= number; index++) {
+            if (index % 3 == 0 && index % 5 == 0) {
+                strResult = "FizzBuzz";
+            } else if (index % 3 == 0) {
+                strResult = "Fizz";
+            } else if (index % 5 == 0) {
+                strResult= "Buzz";
+            } else {
+                strResult = Integer.toString(index);
+            }
+            result[index-1] = strResult;
+        }
+        return result;
+    }
+}
---
+++ JavaPrograms/src/test/java/com/practice/java/interviewcoding/FizzBuzzTest.java
@@ -0,0 +1,24 @@
+package com.practice.java.interviewcoding;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public void init() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    void fizzOrBuzzOrFizzBuzz() {
+        assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"},
+                fizzBuzz.fizzOrBuzzOrFizzBuzz(15));
+        assertArrayEquals(new String[]{"1", "2", "Fizz"}, fizzBuzz.fizzOrBuzzOrFizzBuzz(3));
+        assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz"}, fizzBuzz.fizzOrBuzzOrFizzBuzz(5));
+        assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7"}, fizzBuzz.fizzOrBuzzOrFizzBuzz(7));
+    }
+}
