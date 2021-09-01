12b1c33b45117c3b1af0799d2b01575b3298154f
Chandresh Bhatt <chandreshvbhatt@gmail.com>
2021-09-01 15:41:48 +0530

    Use the stream API

--- JavaPrograms/src/main/java/com/practice/java/arrays/MinimumReduction.java
+++ JavaPrograms/src/main/java/com/practice/java/arrays/MinimumReduction.java
@@ -3,13 +3,15 @@ package com.practice.java.arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
+
 //TODO : JUnit test case to be written.
 public class MinimumReduction {
     public static void main(String[] args) {
         MinimumReduction minimumReduction = new MinimumReduction();
-        //int[] inputArr = {2, 4, 1, 3};
-        //int[] inputArr = {15,11,9,12,5};
-        int[] inputArr = {99, 57, 84, 36};
+        int[] inputArr = {2, 4, 1, 3};   //19
+        //int[] inputArr = {15,11,9,12,5}; //118
+        //int[] inputArr = {99, 57, 84, 36}; //546
         int result1 = minimumReduction.reduceAndSum(inputArr);
         System.out.println("Sum is = " + result1);
     }
@@ -18,9 +20,9 @@ public class MinimumReduction {
         List<Integer> arrList = new ArrayList<>();
         int result;
         int sum = 0;
-        for (Integer item : inputArr) {
-            arrList.add(item);
-        }
+
+        IntStream.range(0, inputArr.length)
+                .forEach(item -> arrList.add(inputArr[item]));
 
         while (arrList.size() > 1) {
             Collections.sort(arrList);
