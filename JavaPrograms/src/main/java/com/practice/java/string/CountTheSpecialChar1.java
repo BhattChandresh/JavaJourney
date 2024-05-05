bfe4fd6234801bfea83266664342c6dc347bd4ad
BhattChandresh <chandreshvbhatt@gmail.com>
2024-05-05 21:10:25 +0530

    Few cosmetic changes

--- JavaPrograms/src/main/java/com/practice/java/string/CountTheSpecialChar1.java
+++ JavaPrograms/src/main/java/com/practice/java/string/CountTheSpecialChar1.java
@@ -1,27 +1,27 @@
 /**
  * You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
  * Return the number of special letters in word.
- * <p>
+
  * Example 1:
  * Input: word = "aaAbcBC"
  * Output: 3
  * Explanation:
  * The special characters in word are 'a', 'b', and 'c'.
- * <p>
+
  * Example 2:
  * Input: word = "abc"
  * Output: 0
  * Explanation:
  * No character in word appears in uppercase.
- * <p>
+
  * Example 3:
  * Input: word = "abBCab"
  * Output: 1
  * Explanation:
  * The only special character in word is 'b'.
- * <p>
+
  * Constraints:
- * <p>
+
  * 1 <= word.length <= 50
  * word consists of only lowercase and uppercase English letters.
  */
