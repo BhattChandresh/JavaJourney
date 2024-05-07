cf462f8c8179c17931b90ccfc63f3dcd98b63f96
BhattChandresh <chandreshvbhatt@gmail.com>
2024-05-07 22:04:44 +0530

    Implement search Insert position - Leetcode 35

--- JavaPrograms/src/main/java/com/practice/java/arrays/SearchInsertPosition.java
+++ JavaPrograms/src/main/java/com/practice/java/arrays/SearchInsertPosition.java
@@ -1,19 +1,19 @@
 /**
  * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
  * You must write an algorithm with O(log n) runtime complexity.
-
+ * <p>
  * Example 1:
  * Input: nums = [1,3,5,6], target = 5
  * Output: 2
-
+ * <p>
  * Example 2:
  * Input: nums = [1,3,5,6], target = 2
  * Output: 1
-
+ * <p>
  * Example 3:
  * Input: nums = [1,3,5,6], target = 7
  * Output: 4
-
+ * <p>
  * Constraints:
  * 1 <= nums.length <= 104
  * -104 <= nums[i] <= 104
@@ -22,6 +22,10 @@
  */
 
 package main.java.com.practice.java.arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SearchInsertPosition {
     public static void main(String[] args) {
@@ -32,8 +36,11 @@ public class SearchInsertPosition {
         //int[] nums = {1,3,5,6};
         //int target = 2;
 
-        int[] nums = {1,3,5,6};
-        int target = 7;
+        //int[] nums = {1, 3, 5, 6};
+        //int target = 7;
+
+        int[] nums = {1,3};
+        int target = 1;
 
 
         SearchInsertPosition insertPosition = new SearchInsertPosition();
@@ -42,22 +49,36 @@ public class SearchInsertPosition {
     }
 
     public int searchInsert(int[] nums, int target) {
-        int length = nums.length;
-        int start = 0;
-        int end = length;
 
-        while (start < end) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+
+        if(!list.contains(target)) {
+            list.add(target);
+            Collections.sort(list);
+        }
+        Integer[] arr = new Integer[list.size()];
+        list.toArray(arr);
+
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
             int middle = (start + end) / 2;
 
-            if (target == nums[middle]) {
+            if (target == arr[middle]) {
                 return middle;
             }
 
-            if (target > nums[middle]) {
+            if (target > arr[middle]) {
                 start = middle + 1;
             }
 
-            if (target < nums[middle]) {
+            if (target < arr[middle]) {
                 end = middle - 1;
             }
         }
